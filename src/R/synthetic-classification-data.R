f43516256d75978bb54dc20bb9148fe5677b96fc
aaron mathankeri <aaron@quantifiedag.com>
2017-08-15 09:01:12 -0500

    Logistic regression is used for BINARY classification. Modified dataset for two classes instead of 3!

--- src/R/synthetic-classification-data.R
+++ src/R/synthetic-classification-data.R
@@ -11,23 +11,20 @@ library(ggplot2)
 # =============================================================================
 # sample from uniform distribution
 # x ~ U(-1,1)
-nSamples <- 30
-xOne <- runif( nSamples, -1, -1) + rnorm( nSamples, 0, 0.2 )
-xTwo <- runif( nSamples, 0, 0) + rnorm( nSamples, 0, 0.2 )
-xThree <- runif( nSamples, 1, 1) + rnorm( nSamples, 0, 0.2 )
-x1 <- c( xOne, xTwo, xThree)
+nSamples <- 10
+xOne <- runif( nSamples, -1, -1) + rnorm( nSamples, 0, 0.1 )
+xTwo <- runif( nSamples, 0, 0) + rnorm( nSamples, 0, 0.1 )
+x1 <- c( xOne, xTwo )
 # =============================================================================
 # Targets
-yOne <- runif( nSamples, -1, -1) + rnorm( nSamples, 0, 0.2 )
-yTwo <- runif( nSamples, 0, 0) + rnorm( nSamples, 0, 0.2 )
-yThree <- runif( nSamples, 1, 1) + rnorm( nSamples, 0, 0.2 )
-x2 <- c( yOne, yTwo, yThree)
+yOne <- runif( nSamples, -1, -1) + rnorm( nSamples, 0, 0.1 )
+yTwo <- runif( nSamples, 0, 0) + rnorm( nSamples, 0, 0.1 )
+x2 <- c( yOne, yTwo )
 # =============================================================================
 # assign labels
 a <- rep( '0', nSamples)
 b <- rep( '1', nSamples)
-c <- rep( '2', nSamples)
-class <- c( a, b, c )
+class <- c( a, b )
 # =============================================================================
 # create dataframe for plots
 df <- cbind.data.frame( x1, x2, class )
@@ -36,6 +33,8 @@ df <- cbind.data.frame( x1, x2, class )
 ggplot(df, aes(x = x1, y = x2)) +
   geom_point(aes(color=class, shape=class))
 # =============================================================================
+# logistic regression in R
+#model <- glm()
 # =============================================================================
 # write to file
 #write(inputs, file = "../data/inputs.txt", ncolumns = 1)
